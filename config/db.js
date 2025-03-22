444b4313d4f69a720f974d03c1bc38da289fd611
Rufus Bright <rufusbright595@gmail.com>
2025-03-22 14:28:21 +0530

    Updated code

--- config/db.js
+++ config/db.js
@@ -1,12 +1,10 @@
-// backend/config/db.js
 const mongoose = require('mongoose');
 
-const mongoURI = process.env.MONGO_URI; // This comes from your .env file
-
+const mongoURI = process.env.MONGO_URI; 
 mongoose
   .connect(mongoURI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => {
     console.error('MongoDB connection error:', err);
-    process.exit(1); // Exit process if unable to connect
+    process.exit(1); 
   });
